96ddcdf901ebdf001306deb06217c3b6e6d14439
Derek Chen <derex1987@gmail.com>
2025-04-12 23:20:19 -0500

    feat: fun startup shutdown messages

--- src/main/java/io/github/derec4/chickenJockey/ChickenJockey.java
+++ src/main/java/io/github/derec4/chickenJockey/ChickenJockey.java
@@ -1,21 +1,28 @@
 package io.github.derec4.chickenJockey;
 
 import io.github.derec4.chickenJockey.entity.ChickenJockeyOnDeath;
-import io.github.derec4.chickenJockey.event.ChickenJockeySpawnEvent;
+import io.github.derec4.chickenJockey.entity.ChickenJockeyOnSpawn;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChickenJockey extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        Bukkit.getPluginManager().registerEvent(new ChickenJockeySpawnEvent(), this);
-        Bukkit.getPluginManager().registerEvent(new ChickenJockeyOnDeath(), this);
+        Bukkit.getPluginManager().registerEvents(new ChickenJockeyOnSpawn(), this);
+        Bukkit.getPluginManager().registerEvents(new ChickenJockeyOnDeath(), this);
+
+        Bukkit.getLogger().info("");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|                             " +
+                "  ");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  | Derex |     CHICKEN JOCKEY v1.1.0");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|     Running on " + Bukkit.getName() + " - " + Bukkit.getVersion());
+        Bukkit.getLogger().info("");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info(ChatColor.RED + "CHICKEN JOCKEY");
     }
 }
